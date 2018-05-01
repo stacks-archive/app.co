79853a7f3539463af1931b2339dba9257c9af402
Thomas Osmonson <thomas.osmonson@gmail.com>
2018-04-30 21:07:28 -0500

    init

---
+++ pages/_app.js
@@ -0,0 +1,26 @@
+import App, { Container } from "next/app";
+import React from "react";
+import { withRouter } from "next/router";
+
+class MyApp extends App {
+  static async getInitialProps({ Component, router, ctx }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <Container>
+        <Component {...pageProps} />
+      </Container>
+    );
+  }
+}
+
+export default withRouter(MyApp);
---
+++ pages/_document.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Document, { Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet, injectGlobal } from "styled-components";
+import { normalize } from "polished";
+/**
+ * Reset our styles
+ */
+injectGlobal`
+${normalize()};
+* {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  box-sizing: border-box;
+}
+`;
+
+export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage(App => props =>
+      sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
+
+  render() {
+    return (
+      <html lang="en">
+        <Head>{this.props.styleTags}</Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
