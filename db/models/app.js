8e2881f13693314e3fef34e301803e6d0cdf2678
hank <hstove@gmail.com>
2018-05-09 08:15:32 -0700

    adds proper enums to app attributes

--- db/models/app.js
+++ db/models/app.js
@@ -1,26 +1,65 @@
+const _ = require('lodash');
+const ENUMS = require('./constants/app-constants');
+
 module.exports = (sequelize, DataTypes) => {
-  const App = sequelize.define(
-    'App',
-    {
-      name: {
-        type: DataTypes.STRING,
-        validate: {
-          notEmpty: true,
-        },
+  const App = sequelize.define('App', {
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: true,
       },
-      category: DataTypes.INTEGER,
-      website: DataTypes.STRING,
-      blockchain: DataTypes.INTEGER,
-      storageNetwork: DataTypes.INTEGER,
-      authentication: DataTypes.INTEGER,
-      openSourceUrl: DataTypes.STRING,
-      registrationIsOpen: DataTypes.BOOLEAN,
-      trackingIsBlocked: DataTypes.BOOLEAN,
     },
-    {},
-  );
+    categoryID: DataTypes.INTEGER,
+    website: DataTypes.STRING,
+    blockchainID: DataTypes.INTEGER,
+    storageNetworkID: DataTypes.INTEGER,
+    authenticationID: DataTypes.INTEGER,
+    openSourceUrl: DataTypes.STRING,
+    registrationIsOpen: DataTypes.BOOLEAN,
+    trackingIsBlocked: DataTypes.BOOLEAN,
+    category: {
+      type: DataTypes.VIRTUAL,
+      get() {
+        return ENUMS.categoryIDToEnum[this.get('categoryID')];
+      },
+      set(value) {
+        this.setDataValue('categoryID', ENUMS.categoryEnums[value]);
+      },
+    },
+    blockchain: {
+      type: DataTypes.VIRTUAL,
+      get() {
+        return ENUMS.blockchainIDToEnum[this.get('blockchainID')];
+      },
+      set(value) {
+        this.setDataValue('blockchainID', ENUMS.blockchainEnums[value]);
+      },
+    },
+    storageNetwork: {
+      type: DataTypes.VIRTUAL,
+      get() {
+        return ENUMS.storageIDToEnum[this.get('storageNetworkID')];
+      },
+      set(value) {
+        this.setDataValue('storageNetworkID', ENUMS.storageEnums[value]);
+      },
+    },
+    authentication: {
+      type: DataTypes.VIRTUAL,
+      get() {
+        return ENUMS.authenticationIDToEnum[this.get('authenticationID')];
+      },
+      set(value) {
+        this.setDataValue('authenticationID', ENUMS.authenticationEnums[value]);
+      },
+    },
+  });
   App.associate = function associations() {
     // associations can be defined here
   };
+
+  _.extend(App, ENUMS);
+
+  // console.log(App.blockchainEnums);
   return App;
 };
--- test/models/apps.test.js
+++ test/models/apps.test.js
@@ -13,3 +13,57 @@ it('requires name to be present', async () => {
 
   expect(app.save).toThrow();
 });
+
+describe('enums', () => {
+  it('uses enums for categories properly', async () => {
+    const app = await App.create({
+      name: 'MyApp',
+      category: 'Social Networking',
+    });
+
+    expect(app.categoryID).toEqual(App.categoryEnums['Social Networking']);
+    expect(app.categoryID).not.toBeNull();
+    expect(app.category).toEqual('Social Networking');
+    app.category = 'Health & Fitness';
+    expect(app.categoryID).toEqual(App.categoryEnums['Health & Fitness']);
+  });
+
+  it('uses enums for blockchains properly', async () => {
+    const app = await App.create({
+      name: 'MyApp',
+      blockchain: 'Ethereum',
+    });
+
+    expect(app.blockchainID).toEqual(App.blockchainEnums.Ethereum);
+    expect(app.blockchainID).not.toBeNull();
+    expect(app.blockchain).toEqual('Ethereum');
+    app.blockchain = 'Bitcoin';
+    expect(app.blockchainID).toEqual(App.blockchainEnums.Bitcoin);
+  });
+
+  it('uses enums for storage properly', async () => {
+    const app = await App.create({
+      name: 'MyApp',
+      storageNetwork: 'Gaia',
+    });
+
+    expect(app.storageNetworkID).toEqual(App.storageEnums.Gaia);
+    expect(app.storageNetworkID).not.toBeNull();
+    expect(app.storageNetwork).toEqual('Gaia');
+    app.storageNetwork = 'IPFS';
+    expect(app.storageNetworkID).toEqual(App.storageEnums.IPFS);
+  });
+
+  it('uses enums for authentication properly', async () => {
+    const app = await App.create({
+      name: 'MyApp',
+      authentication: 'Blockstack',
+    });
+
+    expect(app.authenticationID).toEqual(App.authenticationEnums.Blockstack);
+    expect(app.authenticationID).not.toBeNull();
+    expect(app.authentication).toEqual('Blockstack');
+    app.authentication = 'Ethereum Web3';
+    expect(app.authenticationID).toEqual(App.authenticationEnums['Ethereum Web3']);
+  });
+});
