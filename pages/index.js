ed6e23cf5f5b725ebbe40e5b88f5ee353ca41e4c
hank <hstove@gmail.com>
2018-05-10 19:26:10 -0700

    removes apps list from sidebar

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Hero } from '@containers/hero';
 import { Header } from '@containers/header';
 import { Page } from '@containers/page';
-import { AppList } from '@containers/app-list'
+import { AppList } from '@containers/app-list';
 
 const leftItems = [{ label: "What's a Dapp?" }, { label: 'Who we are' }];
 const rightItems = [{ label: 'Add your Dapp', type: 'button/primary' }];
@@ -12,8 +12,7 @@ const sections = {
   right: rightItems,
 };
 
-export default ({ data }) => { 
- return (
+export default ({ data }) => (
   <Page>
     <Header data={sections} />
     <Hero title="Universal Dapp Repository" />
@@ -21,7 +20,7 @@ export default ({ data }) => {
       <Page.Section.Content>
         <AppList apps={data.apps} show={10}/>
       </Page.Section.Content>
-      <Page.Sidebar>{data.apps.map((app) => <div key={app.id}>{app.name}</div>)}</Page.Sidebar>
+      <Page.Sidebar></Page.Sidebar>
     </Page.Section>
   </Page>
-)};
+);
