80150edc8ccf2f2beefdb7ff095d615fd0c6cc31
hank <hstove@gmail.com>
2018-05-10 19:13:41 -0700

    AppList linting

--- containers/app-list/index.js
+++ containers/app-list/index.js
@@ -1,21 +1,24 @@
 import React from 'react';
+import _ from 'lodash';
+
 import { StyledAppList } from '@components/app-list';
 import { Button } from '@components/button';
-import { LinkButton } from '@components/link-button'
+import { LinkButton } from '@components/link-button';
 
 class AppList extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
+
     this.state = {
       showCount: props.show,
-      showAll: false
-    }
+      showAll: false,
+    };
   }
 
   showAll() {
     this.setState({
-      showAll: true
-    })
+      showAll: true,
+    });
   }
 
   render() {
@@ -23,36 +26,32 @@ class AppList extends React.Component {
     const { showCount, showAll } = this.state;
 
     const renderRows = (apps, showCount, showAll) => {
-      var rank = 1
-      const visibleApps = showAll ? apps : apps.slice(0, showCount)
+      let rank = 1;
+      const visibleApps = showAll ? apps : apps.slice(0, showCount);
       return visibleApps.map((app) => (
         <StyledAppList.Row key={app.id}>
           <StyledAppList.Rank>{rank++}</StyledAppList.Rank>
-          <StyledAppList.Icon>      
-            <StyledAppList.IconImage src='https://blockstack.org/images/logos/app-icon-stealthy-256x256.png' />
+          <StyledAppList.Icon>
+            <StyledAppList.IconImage src="https://blockstack.org/images/logos/app-icon-stealthy-256x256.png" />
           </StyledAppList.Icon>
           <StyledAppList.Name>{app.name}</StyledAppList.Name>
           <StyledAppList.Description>Placeholder for description</StyledAppList.Description>
           <StyledAppList.Category>
-            <StyledAppList.CategoryTag>
-              {app.category}
-            </StyledAppList.CategoryTag>
+            <StyledAppList.CategoryTag>{app.category}</StyledAppList.CategoryTag>
           </StyledAppList.Category>
         </StyledAppList.Row>
-      ))
-    }
+      ));
+    };
 
     if (apps) {
       return (
         <StyledAppList>
           <StyledAppList.Table>
-            <tbody>
-              {renderRows(apps, showCount, showAll)}
-            </tbody>
+            <tbody>{renderRows(apps, showCount, showAll)}</tbody>
           </StyledAppList.Table>
           <StyledAppList.Footer>
             <StyledAppList.ExpandButtonWrapper>
-              <Button onClick={() => {}} type='button/primary'>
+              <Button onClick={() => {}} type="button/primary">
                 Next 100
               </Button>
             </StyledAppList.ExpandButtonWrapper>
@@ -60,11 +59,9 @@ class AppList extends React.Component {
           </StyledAppList.Footer>
         </StyledAppList>
       );
-    } else {
-      return null;
     }
+    return null;
   }
-
-};
+}
 
 export { AppList };
