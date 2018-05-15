bb21bbeda67bc9356aee8622c3036bd3b4de047c
Yukan <yukanliao@gmail.com>
2018-05-15 01:03:40 -0400

    Add featured section.

---
+++ components/featured/index.js
@@ -0,0 +1,122 @@
+import React from 'react';
+import styled from 'styled-components';
+import { wrapperStyles } from '@common/styles';
+
+const StyledFeatured = styled.div`
+  position: relative;
+  width: 100%
+`;
+
+const Wrapper = styled.div`
+  padding: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  @media (max-width: 769px) {
+    padding: 10px 5px;
+  }
+`;
+
+const TitleSection = styled.div`
+  width: 100px;
+  background: linear-gradient(#0CCABA, #0C9AA6);
+  height: 255px;
+  min-width: 145px;
+  color: #fff;
+  flex: 1 1 auto;
+  text-align: left;
+  border-radius: 10px;
+  padding: 15px 15px;
+  margin: 10px 0 0 0;
+  font-weight: bold;
+`
+
+const Section = styled.div`
+  width: 100%;
+  display: flex;
+  flex-flow: row; 
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-around;
+
+  @media (min-width: 769px) {
+    &:not(:first-of-type) {
+      padding-left: 20px;
+    }
+  }
+
+  @media (max-width: 768px) {
+    &:not(:first-of-type) {
+      padding-left: 10px;
+    }
+  }
+`;
+
+const Item = styled.div`
+  display: flex;
+  text-align: left;
+  background: #fff;
+  border-radius: 10px;
+  padding: 15px 15px;
+  width: 30%;
+  min-height: 120px;
+  margin: 10px 1% 0 1%;
+  cursor: pointer;
+`;
+
+const Icon = styled.td`
+  width: 45px;
+  height: 45px;
+`;
+
+const IconImage = styled.img`
+  width: 55px;
+  height: 55px;
+  border-radius: 9px;
+`;
+
+const DefaultIcon = styled.div`
+  width: 55px;
+  height: 55px;
+  color: #ffffff;
+  text-align: center;
+  line-height: 45px;
+  font-size: 20px;
+  background-color: ${(props) => props.bgColor};
+  border-radius: 3px;
+`;
+
+const Name = styled.span`
+  font-weight: bold;
+`;
+
+const NameLink = styled.a`
+  color: #282f36;
+  font-weight: 700;
+  text-decoration: none;
+  padding-left:20px;
+  &:hover {
+    text-decoration: none;
+  }
+`
+
+const Description = styled.p`
+  padding: 5px 20px 5px 20px;
+  margin: 0;
+  font-size: 15px;
+  color: #6c737a;
+`
+
+StyledFeatured.Wrapper = Wrapper;
+StyledFeatured.TitleSection = TitleSection;
+StyledFeatured.Section = Section;
+StyledFeatured.Item = Item;
+StyledFeatured.Icon = Icon;
+StyledFeatured.IconImage = IconImage;
+StyledFeatured.DefaultIcon = DefaultIcon;
+StyledFeatured.Name = Name;
+StyledFeatured.NameLink = NameLink;
+StyledFeatured.Description = Description;
+
+export { StyledFeatured };
--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,33 @@ import React from 'react';
 import { Hero } from '@containers/hero';
 import { Header } from '@containers/header';
 import { Page } from '@containers/page';
+import { Featured } from '@containers/featured';
 import { AppList } from '@containers/app-list';
 // import SubmitDappCard from '@containers/cards/submit-dapp-card';
 import NewsletterCTA from '@containers/newsletter-cta';
+
+const featuredAppIDs = [
+  55,
+  112,
+  52,
+  73,
+  54,
+  50
+  // 48,
+  // 57,
+  // 53
+];
 
 export default ({ data }) => (
   <Page>
     <Header data={data} />
     <Hero />
+    <Page.Section wrap={1}>
+      <Page.Section.Content>
+        <Featured apps={data.apps} featured={featuredAppIDs}/>
+      </Page.Section.Content>
+    </Page.Section>
+
     <Page.Section wrap={1}>
       <Page.Section.Content>
         <AppList apps={data.apps} show={25} />
