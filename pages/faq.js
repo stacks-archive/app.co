87237c5c42d3a8fbd75cdd6a1429c48e7d6bfbdc
Hank Stoever <hstove@gmail.com>
2018-05-30 09:08:29 -0700

    adds copy for rankings in FAQ

--- pages/faq.js
+++ pages/faq.js
@@ -73,6 +73,59 @@ export default ({ data }) => (
           decentralized storage, password-less auth, and a scalable foundation for your dapps. And, Blockstack let’s you
           decouple the personal data you create, from the dapps you use, which makes you the sole owner of that data.
         </p>
+
+        <br />
+        <br />
+        <h2 id="rankings">Rankings on app.co</h2>
+        <p>
+          If you've been browsing the dapps on app.co, you'll notice they are sorted by something we call 'Tweets/Week'.
+          This measure will be evolving over time, but for now, we wanted to give you a little insight into exactly how
+          it works.
+        </p>
+
+        <p>
+          For right now, there are two pieces we add together that make up the Tweets/Week number you see in the
+          right-hand column (which determines rank on the page):
+        </p>
+
+        <ul>
+          <li>The number of tweets that contain the URL of the project</li>
+          <li>The number of tweets that mention the handle of the project</li>
+        </ul>
+
+        <p>
+          We run a query on Twitter for this information every day at 12am EST to collect information on the last 7
+          days.
+        </p>
+
+        <p>
+          Here's a quick example: A project's handle, such as @graphitedocs might have been mentioned in 100 tweets. On
+          top of that, the URL to their site (https://www.graphitedocs.com/) was found in 900 tweets. Their number for
+          Tweets/Week then, would be 1000.
+        </p>
+
+        <p>
+          Note: If a tweet mentions the handle of the project and has the URL, it only counts once. We'll also be
+          introducing logic shortly to eliminate self mentions i.e. the project's handle tweeting it's own URL will not
+          count.
+        </p>
+
+        <p>
+          Bigger picture, we'll be looking to add additional parameters to the rank or offering additional ranking
+          options entirely. For example, the next push will likely be ranking based on traffic to the project's website
+          (based on data from Similar Web). Another example may be sorting by upvotes or other forms of your input.
+        </p>
+
+        <p>
+          We're also working on a program where the dapp creators can give us verified data on their Monthly and Daily
+          Active User counts so you'll be able to sort by that information eventually as well (assuming our pilot with
+          it goes well and is sustainable and accurate).
+        </p>
+
+        <p>
+          We'll keep this post updated as ranking logic is tweaked or other sorting options are introduced. Let us know
+          what you'd like to see or if you have any questions!
+        </p>
       </Page.Section.Content>
     </Page.Section>
   </div>
