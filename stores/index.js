356a446452a95ccb8ebc8b442f567d37b0673866
Hank Stoever <hstove@gmail.com>
2018-06-03 19:03:45 -0700

    re-add persistState

--- stores/index.js
+++ stores/index.js
@@ -9,7 +9,7 @@ import UserStore from '@stores/user';
 
 export default (data) => {
   const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
-  const finalCreateStore = composeEnhancers(applyMiddleware(thunk))(createStore);
+  const finalCreateStore = composeEnhancers(applyMiddleware(thunk), persistState(['user']))(createStore);
 
   const Reducer = combineReducers({
     apps: AppsStore.makeReducer(data),
