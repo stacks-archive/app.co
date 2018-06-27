f3fd29ab728f92cc137dc81ef58948ffb1711989
Thomas Osmonson <thomas.osmonson@gmail.com>
2018-06-26 19:48:11 -0500

    some cleanup of actions and the reducer

--- stores/apps/index.js
+++ stores/apps/index.js
@@ -1,4 +1,3 @@
-import find from 'lodash/find';
 import assignIn from 'lodash/assignIn';
 
 const constants = {
@@ -24,21 +23,19 @@ const savedApp = (app) => ({
   type: constants.SAVED_APP,
   app,
 });
-const saveApp = (data, apiServer, jwt) =>
-  async function innerSaveApp(dispatch) {
-    console.log(data);
-    dispatch(savingApp());
-    const response = await fetch(`${apiServer}/api/admin/apps/${data.id}`, {
-      method: 'POST',
-      headers: new Headers({
-        Authorization: `Bearer ${jwt}`,
-        'Content-Type': 'application/json',
-      }),
-      body: JSON.stringify(data),
-    });
-    const { app } = await response.json();
-    dispatch(savedApp(app));
-  };
+const saveApp = (data, apiServer, jwt) => async (dispatch) => {
+  dispatch(savingApp());
+  const response = await fetch(`${apiServer}/api/admin/apps/${data.id}`, {
+    method: 'POST',
+    headers: new Headers({
+      Authorization: `Bearer ${jwt}`,
+      'Content-Type': 'application/json',
+    }),
+    body: JSON.stringify(data),
+  });
+  const { app } = await response.json();
+  dispatch(savedApp(app));
+};
 
 const fetchingPending = () => ({
   type: constants.FETCHING_PENDING,
@@ -54,32 +51,28 @@ const fetchedAdminApps = (apps) => ({
   apps,
 });
 
-const fetchAdminApps = (apiServer, jwt) =>
-  async function innerFetchAdminApps(dispatch) {
-    const response = await fetch(`${apiServer}/api/admin/apps`, {
-      method: 'GET',
-      headers: new Headers({
-        Authorization: `Bearer ${jwt}`,
-        'Content-Type': 'application/json',
-      }),
-    });
-    const { apps } = await response.json();
-    dispatch(fetchedAdminApps(apps));
-  };
+const fetchAdminApps = (apiServer, jwt) => async (dispatch) => {
+  const response = await fetch(`${apiServer}/api/admin/apps`, {
+    method: 'GET',
+    headers: new Headers({
+      Authorization: `Bearer ${jwt}`,
+      'Content-Type': 'application/json',
+    }),
+  });
+  const { apps } = await response.json();
+  dispatch(fetchedAdminApps(apps));
+};
 
-const fetchPendingApps = (apiServer, jwt) =>
-  async function innerFetchPendingApps(dispatch) {
-    dispatch(fetchingPending());
-    const response = await fetch(`${apiServer}/api/admin/apps/pending`, {
-      headers: new Headers({
-        Authorization: `Bearer ${jwt}`,
-      }),
-    });
-    // console.log(response.json());
-    const { apps } = await response.json();
-    console.log(apps);
-    dispatch(fetchedPending(apps));
-  };
+const fetchPendingApps = (apiServer, jwt) => async (dispatch) => {
+  dispatch(fetchingPending());
+  const response = await fetch(`${apiServer}/api/admin/apps/pending`, {
+    headers: new Headers({
+      Authorization: `Bearer ${jwt}`,
+    }),
+  });
+  const { apps } = await response.json();
+  dispatch(fetchedPending(apps));
+};
 
 const actions = {
   setPlatformFilter,
@@ -113,53 +106,57 @@ const makeReducer = (data) => {
   const reducer = (state = initialState, action) => {
     switch (action.type) {
       case constants.SET_PLATFORM:
-        return Object.assign({}, state, {
+        return {
+          ...state,
           platformFilter: action.platform,
-        });
+        };
       case constants.SELECT_APP: {
         const { id } = action;
-        let app = null;
+        let selectedApp = null;
         if (Number.isInteger(id)) {
-          app = state.apps.find((_app) => _app.id === id);
+          selectedApp = state.apps.find((_app) => _app.id === id);
         } else {
-          app = state.apps.find((_app) => {
+          selectedApp = state.apps.find((_app) => {
             const slug = _app.Slugs.find((_slug) => _slug.value === id);
             return !!slug;
           });
         }
-        return Object.assign({}, state, {
+        return {
+          ...state,
           selectedAppId: id,
-          selectedApp: app,
-        });
+          selectedApp,
+        };
       }
       case constants.SAVING_APP:
-        return Object.assign({}, state, {
+        return {
+          ...state,
           isSavingApp: true,
-        });
+        };
       case constants.SAVED_APP:
-        return Object.assign({}, state, {
+        return {
           isSavingApp: false,
           savedApp: action.app,
           selectedApp: action.app,
-        });
+        };
       case constants.FETCHING_PENDING:
         return Object.assign({}, state, {
           isFetchingPending: true,
         });
       case constants.FETCHED_PENDING:
-        return Object.assign({}, state, {
+        return {
+          ...state,
           isFetchingPending: false,
           pendingApps: action.apps,
-        });
+        };
       case constants.FETCHED_ADMIN_APPS: {
-        console.log('fetched admin apps');
         const newState = { apps: action.apps };
         if (state.selectedAppId) {
-          console.log('existing selectedApp', state.selectedApp);
-          newState.selectedApp = find(action.apps, (app) => app.id === state.selectedAppId);
-          console.log('new selectedApp', newState.selectedApp.status);
+          newState.selectedApp = action.apps.find((app) => app.id === state.selectedAppId);
         }
-        return Object.assign({}, state, newState);
+        return {
+          ...state,
+          ...newState,
+        };
       }
       default:
         return state;
