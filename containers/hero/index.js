cfcd53d60b83f2833b03a3cb310c6cd91add2acc
Hank Stoever <hstove@gmail.com>
2018-06-27 12:28:44 -0700

    change hero text if platform page

--- containers/hero/index.js
+++ containers/hero/index.js
@@ -1,21 +1,41 @@
 import React from 'react';
+
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import { selectPlatformFilter } from '@stores/apps/selectors';
+
 import { StyledHero } from '@components/hero';
 import { StyledHeader } from '@components/header';
 
-const Hero = () => (
+import { capitalize } from '@utils';
+
+const HeroContainer = ({ platformFilter }) => (
   <StyledHero>
-  	<StyledHero.Inner>
-	    <StyledHero.Content>
-	    	<StyledHeader.Logo smShow={true}>App.co</StyledHeader.Logo>
-	      <h1>
-	        Universal Dapp Store
-	      </h1>
-	      <h3>
-	      	Discover Decentralized Apps
-	      </h3>
-	    </StyledHero.Content>
+    <StyledHero.Inner>
+      <StyledHero.Content>
+        <StyledHeader.Logo smShow>App.co</StyledHeader.Logo>
+				{platformFilter ? (
+					<h1>{capitalize(platformFilter)} Apps</h1>
+				) : (
+					<>
+						<h1>Universal Dapp Store</h1>
+						<h3>Discover Decentralized Apps</h3>
+					</>
+				)}
+      </StyledHero.Content>
     </StyledHero.Inner>
   </StyledHero>
 );
+
+const mapStateToProps = (state) => ({
+  platformFilter: selectPlatformFilter(state),
+});
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(Object.assign({}), dispatch);
+}
+
+const Hero = connect(mapStateToProps, mapDispatchToProps)(HeroContainer);
 
 export { Hero };
