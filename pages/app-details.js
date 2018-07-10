7f57aed70f4a131fed133f428dd7342d81e732c9
Hank Stoever <hstove@gmail.com>
2018-07-10 08:14:05 -0700

    mobile app pages, closes #40

--- pages/app-details.js
+++ pages/app-details.js
@@ -3,6 +3,7 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { selectCurrentApp } from '@stores/apps/selectors';
 import Page, { Grid, GridColumn } from '@atlaskit/page';
+import { Flex, Box } from 'grid-styled';
 // import Head from 'next/head';
 import { GithubCircleIcon, TwitterCircleIcon } from 'mdi-react';
 import { Page as Container } from '@containers/page';
@@ -39,106 +40,102 @@ class AppDetails extends React.Component {
       tweets = ranking.twitterMentions || 0;
     }
     return (
-      <div>
-        <Page>
-          <Grid layout="fluid">
-            <GridColumn medium={3}>
-              <Grid>
-                <GridColumn medium={1}>
-                  <AppIcon app={app} />
-                </GridColumn>
-                <GridColumn medium={2}>
-                  <h2>{app.name}</h2>
-                </GridColumn>
-              </Grid>
-              <p>{app.description}</p>
-              <Button
-                type="button/primary"
-                onClick={() => {
-                  outboundLink(app);
-                }}
-              >
-                Visit Website
-              </Button>
-              <br />
-              {app.openSourceUrl &&
-                app.openSourceUrl.indexOf('github.com') !== -1 && (
-                  <>
-                    <StyledApp.BrandLink href={app.openSourceUrl} target="_blank">
-                      <GithubCircleIcon color="currentColor" />
-                      View on Github
-                    </StyledApp.BrandLink>
-                    <br />
-                  </>
-                )}
+      <Flex flexWrap>
+        <Box width={[1, 1, 1/4, 1/4]}>
+          <Flex>
+            <Box width={1/3}>
+              <AppIcon app={app} />
+            </Box>
+            <Box width={2/3}>
+              <h2>{app.name}</h2>
+            </Box>
+          </Flex>
+          <p>{app.description}</p>
+          <Button
+            type="button/primary"
+            onClick={() => {
+              outboundLink(app);
+            }}
+          >
+            Visit Website
+          </Button>
+          <br />
+          {app.openSourceUrl &&
+            app.openSourceUrl.indexOf('github.com') !== -1 && (
+              <>
+                <StyledApp.BrandLink href={app.openSourceUrl} target="_blank">
+                  <GithubCircleIcon color="currentColor" />
+                  View on Github
+                </StyledApp.BrandLink>
+                <br />
+              </>
+            )}
 
-              {app.twitterHandle && (
-                <>
-                  <StyledApp.BrandLink href={`https://twitter.com/${app.twitterHandle}`} target="_blank">
-                    <TwitterCircleIcon color="currentColor" />
-                    View on Twitter
-                  </StyledApp.BrandLink>
-                </>
-              )}
+          {app.twitterHandle && (
+            <>
+              <StyledApp.BrandLink href={`https://twitter.com/${app.twitterHandle}`} target="_blank">
+                <TwitterCircleIcon color="currentColor" />
+                View on Twitter
+              </StyledApp.BrandLink>
+            </>
+          )}
 
-              <br />
-              <StyledApp.MainSection center>
-                <StyledApp.ClaimApp href={`mailto:hello@app.co?subject=I want to claim ${app.name}`}>
-                  Is this your app? Claim it now.
-                </StyledApp.ClaimApp>
-              </StyledApp.MainSection>
-            </GridColumn>
+          <br />
+          <StyledApp.MainSection center>
+            <StyledApp.ClaimApp href={`mailto:hello@app.co?subject=I want to claim ${app.name}`}>
+              Is this your app? Claim it now.
+            </StyledApp.ClaimApp>
+          </StyledApp.MainSection>
+        </Box>
 
-            <GridColumn medium={6}>
-              <StyledApp.MainSection>
-                {app.blockchain && (
-                  <>
-                    <StyledApp.TagLabel>Blockchain</StyledApp.TagLabel>
-                    <StyledAppList.TagGroup left>
-                      <StyledAppList.Tag left>{app.blockchain}</StyledAppList.Tag>
-                    </StyledAppList.TagGroup>
-                    <br />
-                  </>
-                )}
+        <Box width={[1, 1, 1 / 2, 1 / 2]} px={[0, 0, 3, 3]} mt={[2, 2, 0, 0]}>
+          <StyledApp.MainSection>
+            {app.blockchain && (
+              <>
+                <StyledApp.TagLabel>Blockchain</StyledApp.TagLabel>
+                <StyledAppList.TagGroup left>
+                  <StyledAppList.Tag left>{app.blockchain}</StyledAppList.Tag>
+                </StyledAppList.TagGroup>
+                <br />
+              </>
+            )}
 
-                {app.storageNetwork && (
-                  <>
-                    <StyledApp.TagLabel>Storage Network</StyledApp.TagLabel>
-                    <StyledAppList.TagGroup left>
-                      <StyledAppList.Tag left>{app.storageNetwork}</StyledAppList.Tag>
-                    </StyledAppList.TagGroup>
-                    <br />
-                  </>
-                )}
+            {app.storageNetwork && (
+              <>
+                <StyledApp.TagLabel>Storage Network</StyledApp.TagLabel>
+                <StyledAppList.TagGroup left>
+                  <StyledAppList.Tag left>{app.storageNetwork}</StyledAppList.Tag>
+                </StyledAppList.TagGroup>
+                <br />
+              </>
+            )}
 
-                {app.authentication && (
-                  <>
-                    <StyledApp.TagLabel>Authentication</StyledApp.TagLabel>
-                    <StyledAppList.TagGroup left>
-                      <StyledAppList.Tag left>{app.authentication}</StyledAppList.Tag>
-                    </StyledAppList.TagGroup>
-                    <br />
-                  </>
-                )}
+            {app.authentication && (
+              <>
+                <StyledApp.TagLabel>Authentication</StyledApp.TagLabel>
+                <StyledAppList.TagGroup left>
+                  <StyledAppList.Tag left>{app.authentication}</StyledAppList.Tag>
+                </StyledAppList.TagGroup>
+                <br />
+              </>
+            )}
 
-                {app.category && (
-                  <>
-                    <StyledApp.TagLabel>Category</StyledApp.TagLabel>
-                    <StyledAppList.TagGroup left>
-                      <StyledAppList.Tag left>{app.category}</StyledAppList.Tag>
-                    </StyledAppList.TagGroup>
-                  </>
-                )}
-              </StyledApp.MainSection>
-            </GridColumn>
+            {app.category && (
+              <>
+                <StyledApp.TagLabel>Category</StyledApp.TagLabel>
+                <StyledAppList.TagGroup left>
+                  <StyledAppList.Tag left>{app.category}</StyledAppList.Tag>
+                </StyledAppList.TagGroup>
+              </>
+            )}
+          </StyledApp.MainSection>
+        </Box>
 
-            <GridColumn medium={3}>
-              <StyledApp.StatNumber>{tweets}</StyledApp.StatNumber>
-              <StyledApp.StatLabel>Tweets about {app.name} in the last 7 days</StyledApp.StatLabel>
-            </GridColumn>
-          </Grid>
-        </Page>
-      </div>
+        <Box width={[1, 1, 1/4, 1/4]}>
+          <StyledApp.StatNumber>{tweets}</StyledApp.StatNumber>
+          <StyledApp.StatLabel>Tweets about {app.name} in the last 7 days</StyledApp.StatLabel>
+        </Box>
+      </Flex>
     );
   }
 
