e15ab2fe34867c4acd544c5d3323a194f851a592
Hank Stoever <hstove@gmail.com>
2018-07-27 07:33:49 -0700

    re-adds localStorage store

--- stores/index.js
+++ stores/index.js
@@ -2,6 +2,7 @@ import thunk from 'redux-thunk'
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 // import { persistReducer, persistStore } from 'redux-persist';
 // import storage from 'redux-persist/lib/storage';
+import persistState from 'redux-localstorage'
 
 import AppsStore from '@stores/apps'
 import UserStore from '@stores/user'
@@ -9,7 +10,8 @@ import newsletter from '@stores/newsletter'
 
 export default (data) => {
   const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
-  const finalCreateStore = composeEnhancers(applyMiddleware(thunk))(createStore)
+  const persisted = persistState(['user'], { key: 'redux-app-co' })
+  const finalCreateStore = composeEnhancers(applyMiddleware(thunk), persisted)(createStore)
 
   const Reducer = combineReducers({
     apps: AppsStore.makeReducer(data),
