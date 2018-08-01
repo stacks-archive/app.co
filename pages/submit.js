2c7874f57431e243be2657775f9bababb3c12eac
Hank Stoever <hstove@gmail.com>
2018-08-01 10:07:43 -0700

    submit page, #49

--- pages/submit.js
+++ pages/submit.js
@@ -8,9 +8,7 @@ import 'isomorphic-unfetch'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 
-import { Page } from '@containers/page'
-import { Header } from '@containers/header'
-import { Hero } from '@containers/hero'
+import { Page } from '@components/page'
 import { Button } from '@components/button'
 import Form from '@components/form'
 
@@ -33,7 +31,7 @@ class SubmitDapp extends React.Component {
       submitting: false,
       openSourceUrl: '',
       registrationIsOpen: false,
-      twitterHandle: '',
+      twitterHandle: ''
     }
     this.submit = this.submit.bind(this)
   }
@@ -46,9 +44,9 @@ class SubmitDapp extends React.Component {
       method: 'POST',
       headers: {
         Accept: 'application/json, text/plain, */*',
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json'
       },
-      body: JSON.stringify(this.state),
+      body: JSON.stringify(this.state)
     })
     this.setState({ submitted: true, submitting: false })
   }
@@ -83,80 +81,146 @@ class SubmitDapp extends React.Component {
     }
 
     return (
-      <div>
-        <Header data={this.props.data} />
-        <Hero />
-        <Page.Section wrap={1}>
-          <Page.Section.Content>
-            {this.state.submitted || this.state.submitting ? (
-              <p>Thanks for your submission! We'll get back to you soon.</p>
-            ) : (
-              <div>
-                Please share some details. We will review and approve your submission shortly.
-                <Form.Wrapper>
-                  <TextField
-                    value={this.state.name}
-                    onChange={(e) => this.setState({ name: e.target.value })}
-                    label="Dapp Name"
-                  />
-                  <TextField
-                    value={this.state.description}
-                    onChange={(e) => this.setState({ description: e.target.value })}
-                    label="Short description (~50 characters)"
-                    maxLength={50}
-                  />
-                  <TextField
-                    value={this.state.website}
-                    onChange={(e) => this.setState({ website: e.target.value })}
-                    label="Website"
-                  />
-                  <TextField
-                    value={this.state.contact}
-                    onChange={(e) => this.setState({ contact: e.target.value })}
-                    label="Contact Email"
-                  />
-                  <TextField
-                    value={this.state.imageUrl}
-                    onChange={(e) => this.setState({ imageUrl: e.target.value })}
-                    label="Image URL"
-                  />
-                  <TextField
-                    value={this.state.openSourceUrl}
-                    onChange={(e) => this.setState({ openSourceUrl: e.target.value })}
-                    label="Open Source URL"
-                  />
-                  <TextField
-                    value={this.state.twitterHandle}
-                    onChange={(e) => this.setState({ twitterHandle: e.target.value })}
-                    label="Twitter Handle"
-                  />
-                  <br />
-                  <Checkbox
-                    isChecked={this.state.registrationIsOpen}
-                    onChange={() => this.setState({ registrationIsOpen: !this.state.registrationIsOpen })}
-                    label="Registration is open to all users"
-                  />
-                </Form.Wrapper>
-                {enumSelect(appConstants.categoryEnums, 'Category')}
-                {enumSelect(appConstants.blockchainEnums, 'Blockchain')}
-                {enumSelect(appConstants.storageEnums, 'Storage', { apiAttr: 'storageNetwork' })}
-                {enumSelect(appConstants.authenticationEnums, 'Authentication', { menuPlacement: 'top' })}
+      <Page>
+        <Page.Section wrap px py mb={3} richText bg="white">
+          {this.state.submitted || this.state.submitting ? (
+            <p>Thanks for your submission! We'll get back to you soon.</p>
+          ) : (
+            <div style={{marginLeft: 'auto', marginRight: 'auto'}}>
+              Please share some details. We will review and approve your submission shortly.
+              <Form.Wrapper>
+                <TextField
+                  value={this.state.name}
+                  onChange={(e) => this.setState({ name: e.target.value })}
+                  label="Dapp Name"
+                />
+                <TextField
+                  value={this.state.description}
+                  onChange={(e) => this.setState({ description: e.target.value })}
+                  label="Short description (~50 characters)"
+                  maxLength={50}
+                />
+                <TextField
+                  value={this.state.website}
+                  onChange={(e) => this.setState({ website: e.target.value })}
+                  label="Website"
+                />
+                <TextField
+                  value={this.state.contact}
+                  onChange={(e) => this.setState({ contact: e.target.value })}
+                  label="Contact Email"
+                />
+                <TextField
+                  value={this.state.imageUrl}
+                  onChange={(e) => this.setState({ imageUrl: e.target.value })}
+                  label="Image URL"
+                />
+                <TextField
+                  value={this.state.openSourceUrl}
+                  onChange={(e) => this.setState({ openSourceUrl: e.target.value })}
+                  label="Open Source URL"
+                />
+                <TextField
+                  value={this.state.twitterHandle}
+                  onChange={(e) => this.setState({ twitterHandle: e.target.value })}
+                  label="Twitter Handle"
+                />
                 <br />
-                <Button onClick={this.submit} type="button/primary">
-                  Submit
-                </Button>
-              </div>
-            )}
-          </Page.Section.Content>
+                <Checkbox
+                  isChecked={this.state.registrationIsOpen}
+                  onChange={() => this.setState({ registrationIsOpen: !this.state.registrationIsOpen })}
+                  label="Registration is open to all users"
+                />
+              </Form.Wrapper>
+              {enumSelect(appConstants.categoryEnums, 'Category')}
+              {enumSelect(appConstants.blockchainEnums, 'Blockchain')}
+              {enumSelect(appConstants.storageEnums, 'Storage', { apiAttr: 'storageNetwork' })}
+              {enumSelect(appConstants.authenticationEnums, 'Authentication', { menuPlacement: 'top' })}
+              <br />
+              <Button onClick={this.submit} type="button/primary">
+                Submit
+              </Button>
+            </div>
+          )}
         </Page.Section>
-      </div>
+      </Page>
     )
+
+    // return (
+    //   <div>
+    //     <Header data={this.props.data} />
+    //     <Hero />
+    //     <Page.Section wrap={1}>
+    //       <Page.Section.Content>
+    //         {this.state.submitted || this.state.submitting ? (
+    //           <p>Thanks for your submission! We'll get back to you soon.</p>
+    //         ) : (
+    //           <div>
+    //             Please share some details. We will review and approve your submission shortly.
+    //             <Form.Wrapper>
+    //               <TextField
+    //                 value={this.state.name}
+    //                 onChange={(e) => this.setState({ name: e.target.value })}
+    //                 label="Dapp Name"
+    //               />
+    //               <TextField
+    //                 value={this.state.description}
+    //                 onChange={(e) => this.setState({ description: e.target.value })}
+    //                 label="Short description (~50 characters)"
+    //                 maxLength={50}
+    //               />
+    //               <TextField
+    //                 value={this.state.website}
+    //                 onChange={(e) => this.setState({ website: e.target.value })}
+    //                 label="Website"
+    //               />
+    //               <TextField
+    //                 value={this.state.contact}
+    //                 onChange={(e) => this.setState({ contact: e.target.value })}
+    //                 label="Contact Email"
+    //               />
+    //               <TextField
+    //                 value={this.state.imageUrl}
+    //                 onChange={(e) => this.setState({ imageUrl: e.target.value })}
+    //                 label="Image URL"
+    //               />
+    //               <TextField
+    //                 value={this.state.openSourceUrl}
+    //                 onChange={(e) => this.setState({ openSourceUrl: e.target.value })}
+    //                 label="Open Source URL"
+    //               />
+    //               <TextField
+    //                 value={this.state.twitterHandle}
+    //                 onChange={(e) => this.setState({ twitterHandle: e.target.value })}
+    //                 label="Twitter Handle"
+    //               />
+    //               <br />
+    //               <Checkbox
+    //                 isChecked={this.state.registrationIsOpen}
+    //                 onChange={() => this.setState({ registrationIsOpen: !this.state.registrationIsOpen })}
+    //                 label="Registration is open to all users"
+    //               />
+    //             </Form.Wrapper>
+    //             {enumSelect(appConstants.categoryEnums, 'Category')}
+    //             {enumSelect(appConstants.blockchainEnums, 'Blockchain')}
+    //             {enumSelect(appConstants.storageEnums, 'Storage', { apiAttr: 'storageNetwork' })}
+    //             {enumSelect(appConstants.authenticationEnums, 'Authentication', { menuPlacement: 'top' })}
+    //             <br />
+    //             <Button onClick={this.submit} type="button/primary">
+    //               Submit
+    //             </Button>
+    //           </div>
+    //         )}
+    //       </Page.Section.Content>
+    //     </Page.Section>
+    //   </div>
+    // )
   }
 }
 
 const mapStateToProps = (state) => ({
   appConstants: selectAppConstants(state),
-  apiServer: selectApiServer(state),
+  apiServer: selectApiServer(state)
 })
 
 export default connect(mapStateToProps)(SubmitDapp)
