f64490def21522dc09a35b8eac9e3ed7ba23c7f1
Hank Stoever <hstove@gmail.com>
2018-08-02 09:15:48 -0700

    submit newsletter on enter key, j#92

--- components/input/index.js
+++ components/input/index.js
@@ -3,9 +3,16 @@ import { StyledInput } from '@components/input/styled'
 import { ArrowRight } from '@components/svg'
 import { theme } from '@common/styles'
 
+const onKeyDown = (event, action) => {
+  console.log('onkeydown')
+  if (event.key === 'Enter') {
+    action()
+  }
+}
+
 const Input = ({ action, ...rest }) => (
   <StyledInput>
-    <StyledInput.Input {...rest} />
+    <StyledInput.Input {...rest} onKeyDown={(event) => onKeyDown(event, action)} />
     {action ? (
       <StyledInput.Action onClick={() => action()}>
         <ArrowRight color={theme.colors.blue.accent} />
