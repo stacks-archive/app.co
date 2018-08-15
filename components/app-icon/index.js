741005c7bc6a4cc7adfb493eeafc6d77fd693723
Thomas Osmonson <thomas.osmonson@gmail.com>
2018-08-15 10:56:36 -0500

    add in app images

--- components/app-icon/index.js
+++ components/app-icon/index.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import { gradientFromString } from '@common/styles'
 import { StyledAppIcon } from '@components/app-icon/styled'
-
-
+import { Box } from '@components/mining'
 
 const AppIcon = ({ src, alt, size = 60, ...rest }) => (
   <StyledAppIcon size={size} gradient={gradientFromString(alt, 0, 250)} showGradient={!src} {...rest}>
     {src ? (
-      <div>
+      <Box>
         <StyledAppIcon.Image src={src} alt={alt} />
-      </div>
+      </Box>
     ) : null}
   </StyledAppIcon>
 )
