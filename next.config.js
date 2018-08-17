ff46433ba8807cfe38aa640dba9efeb39cc521bb
Hank Stoever <hstove@gmail.com>
2018-08-17 16:09:00 -0700

    webpack bundle analyzer

--- next.config.js
+++ next.config.js
@@ -3,11 +3,24 @@ const webpackConfig = require('./webpack.config')
 const withMDX = require('@zeit/next-mdx')({
   extension: /\.mdx?$/
 })
+const withBundleAnalyzer = require("@zeit/next-bundle-analyzer")
 
-module.exports = withMDX({
+module.exports = withBundleAnalyzer(withMDX({
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
   webpack: (config, { buildId, dev, isServer, defaultLoaders }) => {
     config.resolve = webpackConfig.resolve
     return config
+  },
+  analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
+  analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
+  bundleAnalyzerConfig: {
+    server: {
+      analyzerMode: 'static',
+      reportFilename: './bundles/server.html'
+    },
+    browser: {
+      analyzerMode: 'static',
+      reportFilename: './bundles/client.html'
+    }
   }
-})
+}))
