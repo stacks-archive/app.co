bbeb1b842f29f7be8019601fe0f6d9273f7d76e0
Thomas Osmonson <thomas.osmonson@gmail.com>
2018-08-17 16:59:37 -0500

    Add in function to open modal

--- pages/mining/closing.js
+++ pages/mining/closing.js
@@ -1,16 +1,16 @@
 import * as React from 'react'
 import { Heading, Section } from '@pages/mining/shared'
-import { Box, Img, Flex } from '@components/mining'
+import { Flex } from '@components/mining'
 import { MiningButton } from '@components/mining/button'
 
-const Closing = ({ ...props }) => (
+const Closing = ({ openModal, ...props }) => (
   <>
     <Section minHeight="40vh" justifyContent="flex-end" mt={5} flexDirection="column" {...props}>
       <Heading mb={5} maxWidth={['100%', '500px']}>
         The better your app, the&nbsp;more Bitcoin you earn
       </Heading>
       <Flex maxWidth="960px" width="100%" alignItems="center" justifyContent="center">
-        <MiningButton>Register your app</MiningButton>
+        <MiningButton onClick={() => openModal()}>Register your app</MiningButton>
       </Flex>
     </Section>
   </>
