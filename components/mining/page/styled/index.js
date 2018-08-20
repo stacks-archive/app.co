287cbb71fe86e3f9717e2fa33f3f577add2717dd
Hank Stoever <hstove@gmail.com>
2018-08-20 14:22:46 -0700

    fixes dark body bg

--- components/mining/page/styled/index.js
+++ components/mining/page/styled/index.js
@@ -1,16 +1,15 @@
 import styled, { injectGlobal } from 'styled-components'
 import { Box } from '@components/mining'
 
-injectGlobal`
-html, body{
-  background: #212d37;
-}
-`
 const StyledMiningPage = styled(Box)`
   background: #212d37;
   overflow: hidden;
   min-height: 100vh;
   width: 100%;
+
+  html, body {
+    background: #212d37;
+  }
 
   a {
     &:link,
