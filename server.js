4e775362c77e06a025bd33aede010aa1b8626e3c
Thomas Osmonson <thomas.osmonson@gmail.com>
2018-08-22 09:54:53 +0800

    reenable cookies middleware, clean up server.js

--- server.js
+++ server.js
@@ -22,28 +22,7 @@ const handle = app.getRequestHandler()
 
 const apiServer = process.env.API_SERVER || 'https://app-co-api-staging.herokuapp.com'
 
-// This is where we cache our rendered HTML pages
-
-
-/*
- * NB: make sure to modify this to take into account anything that should trigger
- * an immediate page change (e.g a locale stored in req.session)
- */
-function getCacheKey(req) {
-  return `${req.url}`
-}
-
 async function renderAndCache(req, res, pagePath, serverData) {
-  const key = getCacheKey(req)
-
-  // If we have a page in the cache, let's serve it
-  // if (ssrCache.has(key) && !dev) {
-  //   res.setHeader('x-cache', 'HIT')
-  //   console.log('cache hit')
-  //   res.send(ssrCache.get(key))
-  //   return
-  // }
-
   try {
     const data = await getApps(apiServer)
     data.apiServer = apiServer
@@ -52,20 +31,7 @@ async function renderAndCache(req, res, pagePath, serverData) {
       ...data,
       ...serverData
     }
-
-    // If not let's render the page into HTML
     const html = await app.renderToHTML(req, res, pagePath, dataToPass)
-
-    // Something is wrong with the request, let's skip the cache
-    if (res.statusCode !== 200) {
-      res.send(html)
-      return
-    }
-
-    // Let's cache this page
-    ssrCache.set(key, html)
-    res.setHeader('x-cache', 'MISS')
-    console.log('cache miss')
     res.send(html)
   } catch (err) {
     console.log(err)
@@ -77,7 +43,7 @@ app.prepare().then(() => {
   getApps(apiServer).then((apps) => {
     const server = express()
 
-    // server.use(shrinkRay()).use(cookiesMiddleware())
+    server.use(cookiesMiddleware())
 
     server.set('views', './common/server-views')
     server.set('view engine', 'pug')
@@ -88,80 +54,31 @@ app.prepare().then(() => {
       _next()
     })
 
-    // Use the `renderAndCache` utility defined below to serve pages
-    server.get('/', (req, res) => {
-      renderAndCache(req, res, '/')
-    })
-
-    server.get('/mining', (req, res) => {
-      renderAndCache(req, res, '/mining')
-    })
-
-    server.get('/app/:appSlug', (req, res) => {
-      renderAndCache(req, res, '/')
-    })
-
-    server.get('/apps', (req, res) => {
-      renderAndCache(req, res, '/')
-    })
-
-    server.get('/platforms', (req, res) => {
-      renderAndCache(req, res, '/platforms')
-    })
-    server.get('/platforms/all', (req, res) => {
-      renderAndCache(req, res, '/platforms')
-    })
-
+    server.get('/', (req, res) => renderAndCache(req, res, '/'))
+    server.get('/mining', (req, res) => renderAndCache(req, res, '/mining'))
+    server.get('/app/:appSlug', (req, res) => renderAndCache(req, res, '/'))
+    server.get('/apps', (req, res) => renderAndCache(req, res, '/'))
+    server.get('/platforms', (req, res) => renderAndCache(req, res, '/platforms'))
+    server.get('/platforms/all', (req, res) => renderAndCache(req, res, '/platforms'))
     // redirect to top-level page
-    server.get('/platforms/:platform', (req, res) => {
-      res.redirect(`/${req.params.platform}`)
-    })
-    server.get('/platform/:platform', (req, res) => {
-      res.redirect(`/${req.params.platform}`)
-    })
-
-    server.get('/categories', (req, res) => {
-      renderAndCache(req, res, '/categories')
-    })
-
-    server.get('/categories/all', (req, res) => {
-      renderAndCache(req, res, '/categories/all')
-    })
-    server.get('/categories/:category', (req, res) => {
+    server.get('/platforms/:platform', (req, res) => res.redirect(`/${req.params.platform}`))
+    server.get('/platform/:platform', (req, res) => res.redirect(`/${req.params.platform}`))
+    server.get('/categories', (req, res) => renderAndCache(req, res, '/categories'))
+    server.get('/categories/all', (req, res) => renderAndCache(req, res, '/categories/all'))
+    server.get('/categories/:category', (req, res) =>
       renderAndCache(req, res, '/categories', { category: req.params.category })
-    })
-    server.get('/category/:category', (req, res) => {
-      res.redirect(`/categories/${req.params.category}`)
-    })
-
-    server.get('/faq', (req, res) => {
-      renderAndCache(req, res, '/faq')
-    })
-
-    server.get('/submit', (req, res) => {
-      renderAndCache(req, res, '/submit')
-    })
-
-    server.get('/admin', (req, res) => {
-      renderAndCache(req, res, '/admin')
-    })
-
-    server.get('/admin/app', (req, res) => {
-      renderAndCache(req, res, '/admin/app')
-    })
-
-    server.get('/admin/pending', (req, res) => {
-      renderAndCache(req, res, '/admin/pending')
-    })
-
-    server.get('/all', (req, res) => {
-      renderAndCache(req, res, '/all')
-    })
-
+    )
+    server.get('/category/:category', (req, res) => res.redirect(`/categories/${req.params.category}`))
+    server.get('/faq', (req, res) => renderAndCache(req, res, '/faq'))
+    server.get('/submit', (req, res) => renderAndCache(req, res, '/submit'))
+    server.get('/admin', (req, res) => renderAndCache(req, res, '/admin'))
+    server.get('/admin/app', (req, res) => renderAndCache(req, res, '/admin/app'))
+    server.get('/admin/pending', (req, res) => renderAndCache(req, res, '/admin/pending'))
+    server.get('/all', (req, res) => renderAndCache(req, res, '/all'))
     apps.platforms.forEach((platform) => {
       server.get(`/${slugify(platform)}`, (req, res) => {
         req.params.platform = platform
-        renderAndCache(req, res, '/platforms', { platform })
+        return renderAndCache(req, res, '/platforms', { platform })
       })
     })
 
