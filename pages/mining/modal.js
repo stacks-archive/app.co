e763ffe6540560e6be51c7dd2af60e5c3d4dc5e2
Thomas Osmonson <thomas.osmonson@gmail.com>
2018-10-16 09:10:07 -0500

    update mining modal with some v basic validation, add hover effect to earn links

--- pages/mining/modal.js
+++ pages/mining/modal.js
@@ -2,8 +2,9 @@ import * as React from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { Focus, Input as InputPowerplug, Compose } from 'react-powerplug'
-import { Spring } from 'react-spring'
+import { Spring, animated } from 'react-spring'
 import CloseIcon from 'mdi-react/CloseIcon'
+import AlertBoxIcon from 'mdi-react/AlertBoxIcon'
 import CheckboxBlankCircleOutlineIcon from 'mdi-react/CheckboxBlankCircleOutlineIcon'
 import CheckboxMarkedCircleOutlineIcon from 'mdi-react/CheckboxMarkedCircleOutlineIcon'
 import { Fixed } from 'rebass'
@@ -29,6 +30,12 @@ const Input = ({ focused, ...props }) => (
   />
 )
 
+const CloseButton = ({ ...rest }) => (
+  <Box position="absolute" top={['55px', '35px']} right="20px" {...rest}>
+    <CloseIcon color="white" />
+  </Box>
+)
+
 const Checkbox = ({ label, active, ...props }) => {
   const Icon = active ? CheckboxMarkedCircleOutlineIcon : CheckboxBlankCircleOutlineIcon
   const color = active ? 'white' : 'rgba(255,255,255,0.25)'
@@ -50,12 +57,13 @@ const Checkbox = ({ label, active, ...props }) => {
   )
 }
 
-const Label = ({ ...props }) => <Type.h5 color="white" fontWeight={400} mb={2} {...props} />
+const Label = ({ ...props }) => <Type color="white" fontWeight={400} fontSize={1} mb={2} {...props} />
 
 const AnimatedLabel = ({ focused, position = 'absolute', ...props }) => (
   <Spring native from={{ top: 7, opacity: 0.5 }} to={{ top: focused ? -17 : 7, opacity: focused ? 1 : 0.5 }}>
     {(styles) => (
       <Box
+        is={animated.div}
         style={{
           ...styles,
           pointerEvents: 'none'
@@ -71,25 +79,58 @@ const AnimatedLabel = ({ focused, position = 'absolute', ...props }) => (
 
 const InputGroup = ({ name, label, type, placeholder, onChange, value, autoFocus, ...rest }) => (
   <Compose components={[Focus, InputPowerplug]}>
-    {(focus) =>
-      (
-        <Box mb="35px" position="relative" {...rest}>
-          {label ? <AnimatedLabel focused={focus.focused || value !== ''}>{label}</AnimatedLabel> : null}
-          <Input
-            name={name}
-            type={type}
-            placeholder={placeholder}
-            autoFocus={autoFocus}
-            focused={focus.focused}
-            onChange={onChange}
-            value={value}
-            {...focus.bind}
-          />
-        </Box>
-      )
-    }
+    {({ focused, bind }) => (
+      <Box mb="35px" position="relative" {...rest}>
+        {label ? <AnimatedLabel focused={focused || value !== ''}>{label}</AnimatedLabel> : null}
+        <Input
+          name={name}
+          type={type}
+          placeholder={placeholder}
+          autoFocus={autoFocus}
+          focused={focused}
+          onChange={onChange}
+          value={value}
+          required
+          {...bind}
+        />
+      </Box>
+    )}
   </Compose>
 )
+
+const BackDrop = ({ ...rest }) => (
+  <Flex
+    position="fixed"
+    zIndex={200}
+    left={0}
+    right={0}
+    top={0}
+    bottom={0}
+    bg="rgba(0,0,0,0.5)"
+    alignItems="center"
+    justifyContent="center"
+    {...rest}
+  />
+)
+
+const ModalContainer = ({ ...rest }) => (
+  <Box
+    bg="#212d37"
+    minHeight={['100vh', '0']}
+    minWidth={['100%', '650px']}
+    borderRadius={8}
+    p={4}
+    style={{
+      overflow: 'auto'
+    }}
+    position="relative"
+    maxHeight="100vh"
+    zIndex={205}
+    {...rest}
+  />
+)
+
+const emptyValue = (value) => !value || value === ''
 
 class MiningModalComponent extends React.PureComponent {
   state = {
@@ -98,7 +139,8 @@ class MiningModalComponent extends React.PureComponent {
     lastName: '',
     email: '',
     appName: '',
-    website: ''
+    website: '',
+    error: null
   }
   componentDidMount() {
     document.body.classList.add('no-scroll')
@@ -108,96 +150,122 @@ class MiningModalComponent extends React.PureComponent {
     document.body.classList.remove('no-scroll')
   }
 
-  closeModal() {
+  handleChange = (key, value) => this.setState({ [key]: value })
+
+  validate = (submission) => {
+    let valid = true
+    Object.entries(submission).forEach(([key, value]) => (emptyValue(value) ? (valid = false) : null))
+    return valid
+  }
+
+  closeModal = () => {
+    if (this.state.error) {
+      this.setState({
+        error: null
+      })
+    }
+
     const submission = {
       ...this.state,
       isBlockstackIntegrated: this.state.isBlockstackIntegrated === 'yes'
     }
-    this.props.submitApp(submission, this.props.apiServer)
+
+    if (this.validate(submission)) {
+      this.props.submitApp(submission, this.props.apiServer)
+    }
+    this.setState({ error: 'Please make sure all fields are filled in.' })
   }
 
   render() {
     return (
-      <Flex
-        position="fixed"
-        zIndex={200}
-        left={0}
-        right={0}
-        top={0}
-        bottom={0}
-        bg="rgba(0,0,0,0.5)"
-        alignItems="center"
-        justifyContent="center"
-      >
-        <Box
-          bg="#212d37"
-          minHeight={['100vh', '0']}
-          minWidth={['100%', '650px']}
-          borderRadius={8}
-          p={4}
-          style={{
-            overflow: 'auto'
-          }}
-          position="relative"
-          maxHeight="100vh"
-          zIndex={205}
-        >
-          <Box position="absolute" top={["55px", "35px"]} right="20px" onClick={() => this.props.closeModal()}>
-            <CloseIcon color="white" />
-          </Box>
-          <HeaderType mb={5} mt={[4, 2]} fontSize={4}>
-            Register for App Mining
-          </HeaderType>
-          {this.props.submitting && (
-            <Type.p color="white">Submitting your app...</Type.p>
-          )}
-          {this.props.submitted && (
-            <Type.p color="white">Your app has been submitted! We'll contact you to follow up.</Type.p>
-          )}
-          {!this.props.submitted && !this.props.submitting && (
-            <div>
+      <BackDrop>
+        <ModalContainer>
+          <CloseButton onClick={() => this.props.closeModal()} />
+          <Flex flexDirection="column" mb={4} mt={[4, 2]}>
+            <HeaderType mb={2} fontSize={4}>
+              Register for App Mining
+            </HeaderType>
+            {this.state.error ? (
+              <Flex
+                mt={2}
+                alignItems="center"
+                borderRadius={3}
+                borderColor="rgba(255,255,255,0.1)"
+                border="1px solid"
+                p={3}
+              >
+                <AlertBoxIcon color="#EF6F6F" />
+                <Type fontSize={1} pl={3} color="white">
+                  {this.state.error}
+                </Type>
+              </Flex>
+            ) : null}
+            {this.props.submitting ? <Type color="white">Submitting your app...</Type> : null}
+            {this.props.submitted && (
+              <Type color="white">Your app has been submitted! We'll contact you to follow up.</Type>
+            )}
+          </Flex>
+          {!this.props.submitted && !this.props.submitting ? (
+            <>
               <Flex width={1} flexDirection={['column', 'row']}>
-                <InputGroup label="First Name" name="first-name" flex="1 1 auto" mr={[0, 2]} autoFocus
+                <InputGroup
+                  label="First Name"
+                  name="first-name"
+                  flex="1 1 auto"
+                  mr={[0, 2]}
+                  autoFocus
                   value={this.state.firstName}
-                  onChange={(evt) => this.setState({ firstName: evt.target.value })}
+                  onChange={({ target: { value } }) => this.handleChange('firstName', value)}
                 />
-                <InputGroup label="Last Name" name="last-name" flex="1 1 auto" ml={[0, 3]}
+                <InputGroup
+                  label="Last Name"
+                  name="last-name"
+                  flex="1 1 auto"
+                  ml={[0, 3]}
                   value={this.state.lastName}
-                  onChange={(evt) => this.setState({ lastName: evt.target.value })}
+                  onChange={({ target: { value } }) => this.handleChange('lastName', value)}
                 />
               </Flex>
-              <InputGroup label="Your Email Address" name="email" type="email"
+              <InputGroup
+                label="Your Email Address"
+                name="email"
+                type="email"
                 value={this.state.email}
-                onChange={(evt) => this.setState({ email: evt.target.value })}
+                onChange={({ target: { value } }) => this.handleChange('email', value)}
               />
-              <InputGroup label="Name of your dApp" name="dapp-name"
+              <InputGroup
+                label="Name of your dApp"
+                name="dapp-name"
                 value={this.state.appName}
-                onChange={(evt) => this.setState({ appName: evt.target.value })}
+                onChange={({ target: { value } }) => this.handleChange('appName', value)}
               />
-              <InputGroup label="dApp Website" name="dapp-website" type="url"
+              <InputGroup
+                label="dApp Website"
+                name="dapp-website"
+                type="url"
                 value={this.state.website}
-                onChange={(evt) => this.setState({ website: evt.target.value })}
+                onChange={({ target: { value } }) => this.handleChange('website', value)}
               />
               <Label>Does your app have Blockstack Auth integrated?</Label>
-              <Box onClick={() => this.setState({ isBlockstackIntegrated: 'yes' })}>
+              <Box onClick={() => this.handleChange('isBlockstackIntegrated', 'yes')}>
                 <Checkbox label="Yes" active={this.state.isBlockstackIntegrated === 'yes'} />
               </Box>
-              <Box onClick={() => this.setState({ isBlockstackIntegrated: 'no' })}>
+              <Box onClick={() => this.handleChange('isBlockstackIntegrated', 'no')}>
                 <Checkbox label="No" active={this.state.isBlockstackIntegrated === 'no'} />
               </Box>
               <Flex pt={4}>
                 <MiningButton onClick={() => this.closeModal()}>Submit</MiningButton>
               </Flex>
-            </div>
-          )}
-        </Box>
+            </>
+          ) : null}
+        </ModalContainer>
         <Fixed left={0} zIndex={201} right={0} top={0} bottom={0} onClick={() => this.props.closeModal()} />
-      </Flex>
+      </BackDrop>
     )
   }
 }
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   submitting: state.mining.isSubmitting,
   submitted: state.mining.hasSubmitted,
   apiServer: selectApiServer(state)
@@ -207,6 +275,9 @@ function mapDispatchToProps(dispatch) {
   return bindActionCreators(Object.assign({}, MiningActions), dispatch)
 }
 
-const MiningModal = connect(mapStateToProps, mapDispatchToProps)(MiningModalComponent)
+const MiningModal = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MiningModalComponent)
 
 export { MiningModal }
