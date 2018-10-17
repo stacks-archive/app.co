daf5aab841837bb11c5729e6109e5acc958f0162
Thomas Osmonson <thomas.osmonson@gmail.com>
2018-10-17 11:58:45 -0500

    add app.co url in header #280

--- pages/mining/index.js
+++ pages/mining/index.js
@@ -18,7 +18,13 @@ const Header = () => (
   <Section>
     <HeaderType mr={2}>App Mining by</HeaderType> <AppIcon size={32} m={2} />
     <HeaderType ml={2}>
-      App.co and <a href="https://blockstack.org">Blockstack</a>
+      <a href="https://app.co" target="_blank">
+        App.co
+      </a>{' '}
+      and{' '}
+      <a href="https://blockstack.org" target="_blank">
+        Blockstack
+      </a>
     </HeaderType>
   </Section>
 )
