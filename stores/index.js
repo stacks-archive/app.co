3cdb9fe3a14a7663894e6d9ce35031986d08c6b2
Thomas Osmonson <thomas.osmonson@gmail.com>
2018-10-17 13:28:16 -0500

    uncomment out persistState

--- stores/index.js
+++ stores/index.js
@@ -1,7 +1,6 @@
 import thunk from 'redux-thunk'
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
-// import { persistReducer, persistStore } from 'redux-persist';
-// import storage from 'redux-persist/lib/storage';
+import persistState from 'redux-localstorage'
 
 import AppsStore from '@stores/apps'
 import UserStore from '@stores/user'
@@ -12,9 +11,9 @@ import AdminMiningReducer from '@stores/mining-admin/reducer'
 
 export default (data) => {
   const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
-  // const persisted = persistState(['user'], { key: 'redux-app-co' })
-  // const finalCreateStore = composeEnhancers(applyMiddleware(thunk), persisted)(createStore)
-  const finalCreateStore = composeEnhancers(applyMiddleware(thunk))(createStore)
+
+  const persisted = persistState(['user'], { key: 'redux-app-co' })
+  const finalCreateStore = composeEnhancers(applyMiddleware(thunk), persisted)(createStore)
 
   const Reducer = combineReducers({
     apps: AppsStore.makeReducer(data),
