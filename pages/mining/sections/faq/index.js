ae21019fd0703ee7a13169a2e41d8d69ff51badb
Thomas Osmonson <thomas.osmonson@gmail.com>
2018-12-11 11:42:03 -0600

    revert faq colors

--- pages/mining/sections/faq/index.js
+++ pages/mining/sections/faq/index.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import { Flex, Box, Type, Button } from 'blockstack-ui'
+import { Flex, Box, Type } from 'blockstack-ui'
 import { Title, Wrapper, Section } from '@components/mining/shared'
 import { slugify } from '@utils'
 import { LinkVariantIcon as LinkIcon } from 'mdi-react'
-import { Hover, State } from 'react-powerplug'
+import { Hover } from 'react-powerplug'
 
 const FAQSection = ({ faq, apps, ...rest }) => (
-  <Section pt={8} bg="blue.dark" {...rest}>
+  <Section bg="blue.light" pt={8} {...rest}>
     <Wrapper>
       <Flex width={[1]} flexShrink={0} flexDirection="column">
         <Title maxWidth="100%" pt={8} pb={0}>
           Frequently Asked Questions
         </Title>
-        <Box width={1} mt={8} color="white" pl={4} borderLeft={'5px solid'} borderColor="blue.darker">
+        <Box width={1} mt={8} color="blue.dark" pl={4} borderLeft={'5px solid'} borderColor="blue.mid">
           {faq.map(({ question }, i) => (
             <React.Fragment key={i}>
               <Box py={'6px'} lineHeight={1.5}>
@@ -26,7 +26,7 @@ const FAQSection = ({ faq, apps, ...rest }) => (
                       href={`#${slugify(question)}`}
                       {...bind}
                     >
-                      <Type fontSize={2} fontWeight="500" color={hovered ? 'white' : 'blue.mid'}>
+                      <Type fontSize={2} fontWeight="500" color={hovered ? 'blue' : 'blue.dark'}>
                         {question}
                       </Type>
                     </Flex>
@@ -36,8 +36,8 @@ const FAQSection = ({ faq, apps, ...rest }) => (
             </React.Fragment>
           ))}
         </Box>
-        <Box mt={9} width={80} height={'1px'} bg={'blue.accent'} />
-        <Box width={1} pt={5} color="white">
+        <Box mt={9} width={80} height={'1px'} bg={'blue.mid'} />
+        <Box width={1} pt={5} color="blue.dark">
           {faq.map(({ question, answer }, i) => (
             <React.Fragment key={i}>
               <Box id={slugify(question)} pt={7}>
@@ -54,10 +54,10 @@ const FAQSection = ({ faq, apps, ...rest }) => (
                     </Flex>
                   )}
                 </Hover>
-                <Box lineHeight={1.65} color="blue.mid">
+                <Box lineHeight={1.65} color="blue.dark">
                   <Type maxWidth={900} dangerouslySetInnerHTML={{ __html: answer }} />
                 </Box>
-                <Box mt={6} width={80} height={'1px'} bg={'blue.accent'} />
+                <Box mt={6} width={80} height={'1px'} bg={'blue.mid'} />
               </Box>
             </React.Fragment>
           ))}
