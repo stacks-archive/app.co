1f7b4d4a141f1d7abb6b0b898b6b678b9594da2e
Thomas Osmonson <thomas.osmonson@gmail.com>
2019-04-03 09:58:18 -0500

    update deps, add two new reviewers and logos

--- pages/mining/sections/faq/index.js
+++ pages/mining/sections/faq/index.js
@@ -4,76 +4,85 @@ import { Title, Wrapper, ObservedSection as Section } from '@components/mining/s
 import { slugify } from '@utils'
 import { LinkVariantIcon as LinkIcon } from 'mdi-react'
 import { Hover } from 'react-powerplug'
+import styled from 'styled-components'
+
+const IframeStyles = styled.div`
+  iframe {
+    max-width: 100%;
+  }
+`
 
 const FAQSection = ({ faq, apps, ...rest }) => (
-  <Section
-    bg="blue.light"
-    pt={8}
-    inViewProps={{
-      threshold: 0
-    }}
-    {...rest}
-  >
-    {({ inView }) => (
-      <Wrapper maxWidth={820} inView={inView} observed>
-        <Flex width={[1]} flexShrink={0} flexDirection="column">
-          <Title textAlign="center" maxWidth="80%" mx="auto" pt={[0, 8]} pb={0}>
-            Frequently Asked Questions
-          </Title>
-          <Box width={1} mt={8} color="blue.dark" pl={4} borderLeft={'5px solid'} borderColor="blue.mid">
-            {faq.map(({ question }, i) => (
-              <React.Fragment key={i}>
-                <Box py={'6px'} lineHeight={1.5}>
-                  <Hover>
-                    {({ hovered, bind }) => (
-                      <Flex
-                        style={{
-                          textDecoration: 'none'
-                        }}
-                        is="a"
-                        href={`#${slugify(question)}`}
-                        {...bind}
-                      >
-                        <Type fontSize={2} fontWeight="500" color={hovered ? 'blue' : 'blue.dark'}>
-                          {question}
-                        </Type>
-                      </Flex>
-                    )}
-                  </Hover>
-                </Box>
-              </React.Fragment>
-            ))}
-          </Box>
-          <Box mt={9} width={80} height={'1px'} bg={'blue.mid'} />
-          <Box width={1} pt={5} color="blue.dark">
-            {faq.map(({ question, answer }, i) => (
-              <React.Fragment key={i}>
-                <Box id={slugify(question)} pt={7}>
-                  <Hover>
-                    {({ hovered, bind }) => (
-                      <Flex {...bind}>
-                        <Type pb={5} fontSize={4} fontWeight="500">
-                          {question}
-                        </Type>
-
-                        <Box is="a" href={`#${slugify(question)}`} opacity={hovered ? 1 : 0} pl={2}>
-                          <LinkIcon />
-                        </Box>
-                      </Flex>
-                    )}
-                  </Hover>
-                  <Box lineHeight={1.65} color="blue.dark">
-                    <Type maxWidth={900} dangerouslySetInnerHTML={{ __html: answer }} />
+  <IframeStyles>
+    <Section
+      bg="blue.light"
+      pt={8}
+      inViewProps={{
+        threshold: 0
+      }}
+      {...rest}
+    >
+      {({ inView }) => (
+        <Wrapper maxWidth={820} inView={inView} observed>
+          <Flex width={[1]} flexShrink={0} flexDirection="column">
+            <Title textAlign="center" maxWidth="80%" mx="auto" pt={[0, 8]} pb={0}>
+              Frequently Asked Questions
+            </Title>
+            <Box width={1} mt={8} color="blue.dark" pl={4} borderLeft={'5px solid'} borderColor="blue.mid">
+              {faq.map(({ question }, i) => (
+                <React.Fragment key={i}>
+                  <Box py={'6px'} lineHeight={1.5}>
+                    <Hover>
+                      {({ hovered, bind }) => (
+                        <Flex
+                          style={{
+                            textDecoration: 'none'
+                          }}
+                          is="a"
+                          href={`#${slugify(question)}`}
+                          {...bind}
+                        >
+                          <Type fontSize={2} fontWeight="500" color={hovered ? 'blue' : 'blue.dark'}>
+                            {question}
+                          </Type>
+                        </Flex>
+                      )}
+                    </Hover>
                   </Box>
-                  <Box mt={6} width={80} height={'1px'} bg={'blue.mid'} />
-                </Box>
-              </React.Fragment>
-            ))}
-          </Box>
-        </Flex>
-      </Wrapper>
-    )}
-  </Section>
+                </React.Fragment>
+              ))}
+            </Box>
+            <Box mt={9} width={80} height={'1px'} bg={'blue.mid'} />
+            <Box width={1} pt={5} color="blue.dark">
+              {faq.map(({ question, answer }, i) => (
+                <React.Fragment key={i}>
+                  <Box id={slugify(question)} pt={7}>
+                    <Hover>
+                      {({ hovered, bind }) => (
+                        <Flex {...bind}>
+                          <Type pb={5} fontSize={4} fontWeight="500">
+                            {question}
+                          </Type>
+
+                          <Box is="a" href={`#${slugify(question)}`} opacity={hovered ? 1 : 0} pl={2}>
+                            <LinkIcon />
+                          </Box>
+                        </Flex>
+                      )}
+                    </Hover>
+                    <Box lineHeight={1.65} color="blue.dark">
+                      <Type maxWidth={900} dangerouslySetInnerHTML={{ __html: answer }} />
+                    </Box>
+                    <Box mt={6} width={80} height={'1px'} bg={'blue.mid'} />
+                  </Box>
+                </React.Fragment>
+              ))}
+            </Box>
+          </Flex>
+        </Wrapper>
+      )}
+    </Section>
+  </IframeStyles>
 )
 
 export { FAQSection }
--- pages/mining/sections/ranking/index.js
+++ pages/mining/sections/ranking/index.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import { Flex, Box, Type } from 'blockstack-ui'
-import { Title, Wrapper, Section, ObservedSection } from '@components/mining/shared'
-import { Dots, DotsLine, DemoEarthLogo, ProductHuntLogo, CameraIcon, TryMyUILogo } from '@components/mining/svg'
-import { Hover } from 'react-powerplug'
+import { Title, Wrapper, ObservedSection } from '@components/mining/shared'
+import { useHover } from 'use-events'
+import {
+  Dots,
+  DotsLine,
+  DemoEarthLogo,
+  ProductHuntLogo,
+  CameraIcon,
+  TryMyUILogo,
+  NewInternetLabsHorizontal,
+  AwarioLogo
+} from '@components/mining/svg'
 import { animated } from 'react-spring'
 
 const texts = [
@@ -41,45 +50,58 @@ const TextSection = ({ ...rest }) => (
   </Flex>
 )
 
-const Ranker = ({ position, logo: Logo, children, color, logoProps = {}, ...rest }) => (
-  <Hover>
-    {({ hovered, bind }) => (
-      <Box
-        overflow="hidden"
-        ml={[0, 0, 5, 5]}
-        bg="#081537"
-        width={[1, 1, '28.333%', '28.333%']}
-        flexGrow={1}
-        border={1}
-        borderColor={color}
-        minHeight={120}
-        pl={4}
-        pr={[4, 4, 4, 7]}
-        pt={4}
-        pb={6}
-        mt={[4, 4, 0, 0]}
-        position="relative"
-        zIndex={5}
-        textAlign="left"
-        transition="0.1s all ease-in-out"
-        transform={hovered ? 'translateY(-6px)' : 'none'}
-        boxShadow={hovered ? `${color} 0px 0px 15px` : `transparent 0px 0px 10px`}
-        {...bind}
-        {...rest}
-      >
-        {Logo ? (
-          <Box color="white" maxWidth={100} pb={2} {...logoProps}>
-            <Logo />
-          </Box>
-        ) : null}
-        <Type pb={2} pt={[2, 2, 0, 0]} color={color}>
-          {children}
-        </Type>
-        <DotsAnimation position={position} hovered={hovered} color={color} />
+const RankerContainer = ({
+  position,
+  logo: Logo,
+  children,
+  color,
+  logoProps = {},
+  hovered,
+  textColor = color,
+  ...rest
+}) => (
+  <Box
+    overflow="hidden"
+    ml={[0, 0, 5, 5]}
+    bg="#081537"
+    width={[1, 1, '28.333%', '28.333%']}
+    flexGrow={1}
+    border={1}
+    borderColor={color}
+    minHeight={120}
+    pl={4}
+    pr={[4, 4, 4, 7]}
+    pt={4}
+    pb={6}
+    mt={[4]}
+    position="relative"
+    zIndex={5}
+    textAlign="left"
+    transition="0.1s all ease-in-out"
+    transform={hovered ? 'translateY(-5px)' : 'translateY(0px)'}
+    style={{ willChange: 'transform' }}
+    {...rest}
+  >
+    {Logo ? (
+      <Box color="white" maxWidth={100} pb={2} {...logoProps}>
+        <Logo />
       </Box>
-    )}
-  </Hover>
+    ) : null}
+    <Type transition="0.1s all ease-in-out" pb={2} pt={[2, 2, 0, 0]} color={hovered ? 'white' : textColor}>
+      {children}
+    </Type>
+    <DotsAnimation position={position} hovered={hovered} color={color} />
+  </Box>
 )
+
+const Ranker = ({ children, ...rest }) => {
+  const [hovered, bind] = useHover()
+  return (
+    <RankerContainer {...rest} {...bind} hovered={hovered}>
+      {children}
+    </RankerContainer>
+  )
+}
 
 const RankingSection = ({ apps, ...rest }) => (
   <ObservedSection bg="blue.dark" {...rest}>
@@ -131,21 +153,21 @@ const RankingSection = ({ apps, ...rest }) => (
               overflow="hidden"
               flexWrap="wrap"
               lineHeight={1.5}
-              p={6}
+              pb={6}
               bg="#081537"
               position="relative"
               pl={[5, 5, 5, 7]}
-              pr={5}
-              pt={[7, 7, 6, 6]}
+              pr={6}
+              pt={[4]}
             >
               <Ranker
                 is="a"
                 href="https://blog.producthunt.com/only-the-best-dapps-were-joining-blockstack-s-app-reviewer-program-%EF%B8%8F-6085bea0f501"
                 target="_blank"
                 logo={ProductHuntLogo}
-                mt={0}
                 color="#da552f"
-                position={-70}
+                key="ph"
+                position={-6}
                 logoProps={{
                   minWidth: [180, 150, 150, 180]
                 }}
@@ -154,6 +176,7 @@ const RankingSection = ({ apps, ...rest }) => (
                 <Box is="br" display={['none', 'none', 'none', 'unset']} /> community upvotes and activity.
               </Ranker>
               <Ranker
+                key="de"
                 is="a"
                 href="https://words.democracy.earth/democratic-app-ranking-democracy-earth-to-represent-blockstack-community-vote-on-app-mining-7ec8360bdc30"
                 target="_blank"
@@ -168,6 +191,7 @@ const RankingSection = ({ apps, ...rest }) => (
                 <Box is="br" display={['none', 'none', 'none', 'unset']} /> investor community.
               </Ranker>
               <Ranker
+                key="try"
                 is="a"
                 href="https://www.trymyui.com/blog/2019/01/09/trymyui-partners-with-blockstack-to-rate-blockchain-based-apps/"
                 target="_blank"
@@ -177,6 +201,33 @@ const RankingSection = ({ apps, ...rest }) => (
               >
                 Ranks by user testing
                 <Box is="br" display={['none', 'none', 'none', 'unset']} /> and usability metrics.
+              </Ranker>
+              <Ranker
+                key="aw"
+                is="a"
+                href="https://blog.blockstack.org/introducing-awario-app-reviewer/"
+                target="_blank"
+                logo={AwarioLogo}
+                color="#0bb3ff"
+                position={-5}
+              >
+                Ranks by 'awareness' level
+                <Box is="br" display={['none', 'none', 'none', 'unset']} /> based on mentions and reach.
+              </Ranker>
+              <Ranker
+                key="nil"
+                is="a"
+                href="https://blog.blockstack.org/introducing-new-internet-labs-the-digital-rights-reviewer-for-app-mining/"
+                target="_blank"
+                logo={NewInternetLabsHorizontal}
+                logoProps={{
+                  minWidth: [200, 152, 152, 200]
+                }}
+                color="#b077ff"
+                position={-25}
+              >
+                Ranks based on how well they protect <Box is="br" display={['none', 'none', 'none', 'unset']} />
+                the fundamental digital rights of users.
               </Ranker>
               <Box left={35} top={0} position="absolute">
                 <DotsLine />
