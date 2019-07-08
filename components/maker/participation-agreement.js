62f4a8d9902c4167261c7fac5afb319c66667fcd
Hank Stoever <hstove@gmail.com>
2019-07-08 15:25:10 -0700

    adds 'is USA' checkbox on participation agreement

--- components/maker/participation-agreement.js
+++ components/maker/participation-agreement.js
@@ -5,13 +5,14 @@ import Notification from './notification'
 const ParticipationAgreement = ({ app, apiServer, accessToken, display }) => {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
+  const [isUSA, setIsUSA] = useState(true)
   const [document, setDocument] = useState(null)
   const [loading, setLoading] = useState(false)
   const [finished, setFinished] = useState(!!app.hasAcceptedSECTerms)
 
   const getDocument = async () => {
     setLoading(true)
-    const url = `${apiServer}/api/maker/make-participation-agreement?accessToken=${accessToken}&name=${name}&email=${email}`
+    const url = `${apiServer}/api/maker/make-participation-agreement?accessToken=${accessToken}&name=${name}&email=${email}&isUSA=${isUSA}`
     const response = await fetch(url, {
       method: 'POST'
     })
@@ -66,12 +67,34 @@ const ParticipationAgreement = ({ app, apiServer, accessToken, display }) => {
                 </Type>
                 <Type mb={4}>Provide your name and email address below to start the signing process.</Type>
                 <Field name="name" label="Your Name" onChange={(e) => setName(e.target.value)} value={name} />
-                <Field
-                  name="stacksAddress"
-                  label="Your Email"
-                  onChange={(e) => setEmail(e.target.value)}
-                  value={email}
-                />
+                  <Box pb={4}>
+                    <Flex pb={3} alignItems="center">
+                      <input
+                        type="checkbox"
+                        name="isUSA"
+                        id="isUSA"
+                        checked={isUSA}
+                        onChange={(e) =>
+                          setIsUSA(e.target.checked)
+                        }
+                      />
+                      <Field.LabelAdvanced
+                        labelProps={{
+                          pb: 0,
+                          htmlFor: "isUSA"
+                        }}
+                        pl={2}
+                        label="I am based in the USA"
+                      />
+                    </Flex>
+                  </Box>
+                {/* <Field
+                  name="isUSA"
+                  label="I am based in the United States"
+                  onChange={(e) => setIsUSA(e.target.checked)}
+                  type="checkbox"
+                  value={isUSA}
+                /> */}
                 <Button mt={4} disabled={loading} onClick={() => getDocument()}>
                   {loading ? 'Starting...' : 'Start Signing Process'}
                 </Button>
