28e1052f67d175daed5b843876a7388549ef8140
Hank Stoever <hstove@gmail.com>
2019-08-21 11:29:51 -0700

    use aggregators for api calls

--- common/lib/api.js
+++ common/lib/api.js
@@ -1,51 +1,12 @@
-const request = require('request-promise')
-
-const { ssrCache } = require('./cache')
 const AppsAggregator = require('./aggregators/apps')
+const AppMiningMonths = require('./aggregators/mining-months')
+const RankedApps = require('./aggregators/ranked-apps')
 
-const dev = process.env.NODE_ENV !== 'production'
-const API_CACHE_KEY = 'API_CACHE_KEY'
-const BLOCKSTACK_RANKED_APPS_KEY = 'BLOCKSTACK_RANKED_APPS_KEY'
-const MINING_RESULTS_KEY = 'MINING_RESULTS_KEY'
+const getApps = () => AppsAggregator.fetch()
 
-const getApps = async () => AppsAggregator.fetch()
+const getAppMiningMonths = () => AppMiningMonths.fetch()
 
-const getAppMiningMonths = (apiServer) =>
-  new Promise(async (resolve, reject) => {
-    try {
-      if (await ssrCache.has(MINING_RESULTS_KEY)) {
-        return resolve(JSON.parse(await ssrCache.getAsync(MINING_RESULTS_KEY)))
-      }
-      const { months } = await request({
-        uri: `${apiServer}/api/app-mining-months`,
-        json: true
-      })
-
-      await ssrCache.setAsync(MINING_RESULTS_KEY, JSON.stringify(months))
-      return resolve(months)
-    } catch (error) {
-      console.log('Error when fetching mining months', error)
-      return reject(error)
-    }  
-  })
-
-const getRankedBlockstackApps = async (api) => {
-  try {
-    if (await ssrCache.has(BLOCKSTACK_RANKED_APPS_KEY)) {
-      return JSON.parse(await ssrCache.getAsync(BLOCKSTACK_RANKED_APPS_KEY))
-    }
-    const months = await getAppMiningMonths(api)
-
-    const rankings = months[months.length - 1].compositeRankings
-
-    await ssrCache.setAsync(BLOCKSTACK_RANKED_APPS_KEY, JSON.stringify(rankings))
-    return rankings
-  } catch (error) {
-    console.error('error when fetching ranked apps', error)
-    return []
-  }
-  
-}
+const getRankedBlockstackApps = () => RankedApps.fetch()
 
 module.exports = {
   getApps,
