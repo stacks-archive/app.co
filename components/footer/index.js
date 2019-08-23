a83e574a85ed11540643de0d51662d912c95abe3
Hank Stoever <hstove@gmail.com>
2019-08-23 10:05:58 -0700

    slightly better footer style

--- components/footer/index.js
+++ components/footer/index.js
@@ -20,7 +20,7 @@ const Footer = ({ isErrorPage, admin, ...props }) => (
       </Box>
     </Flex>
     <Flex mt={5}>
-      <Box width={1} px={[2, 4, 8]}>
+      <Box width={1} px={[2, 4, 8]} fontSize={1} py={6}>
         <Type textAlign="center">Blockstack is not registered, licensed or supervised as a broker dealer or investment adviser by the Securities and Exchange Commission (SEC), the Financial Industry Regulatory Authority (FINRA) or any other financial regulatory authority or licensed to provide any financial advice or services.</Type>
       </Box>
     </Flex>
