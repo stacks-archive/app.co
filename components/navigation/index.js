472baf2aabc1392df610708975c4bda3d504eb86
Hank Stoever <hstove@gmail.com>
2019-09-16 16:42:52 -0700

    fixes error message from invalid href

--- components/navigation/index.js
+++ components/navigation/index.js
@@ -35,9 +35,7 @@ const NavigationComponent = ({ handleClick, isErrorPage, openNewsletterModal, fo
           <ErrorPageLink isErrorPage={isErrorPage} href="/mining" prefetch {...extraProps}>
             <a href="/mining">App Mining</a>
           </ErrorPageLink>
-          <ErrorPageLink isErrorPage={isErrorPage} href="https://blog.app.co" {...extraProps}>
-            <a href="https://blog.app.co">Blog</a>
-          </ErrorPageLink>
+          <a href="https://blog.app.co" target="_blank" rel="noopener noreferrer">Blog</a>
           <a
             href="#"
             onClick={(evt) => {
