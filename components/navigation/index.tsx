82cee6d1caac49bc288fdf30c27150882335f739
kyranjamie <kyran.burraston@gmail.com>
2019-11-05 17:26:50 +0100

    refactor: Include eslinting on TypeScript code

--- components/navigation/index.tsx
+++ components/navigation/index.tsx
@@ -3,16 +3,15 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
 import NewsletterActions from '@stores/newsletter/actions'
-import { StyledTopBar, StyledAnchor } from '@components/top-bar/styled'
+import { StyledTopBar } from '@components/top-bar/styled'
 
-const NavigationComponent = ({ children, mobile, display, variant }) => {
+const NavigationComponent: React.FC<any> = ({ children, mobile, display, variant }) => {
   return (
     <StyledTopBar.Navigation display={display} mobile={mobile} variant={variant}>
       {children}
     </StyledTopBar.Navigation>
   )
 }
-
 
 const mapDispatchToProps = dispatch => bindActionCreators({ ...NewsletterActions }, dispatch)
 
