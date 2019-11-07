98b345e1ae1c491f99d997db92cac070a3720150
kyranjamie <kyran.burraston@gmail.com>
2019-11-07 10:51:57 +0100

    feat: Add App model interface

--- components/app-directory/index.tsx
+++ components/app-directory/index.tsx
@@ -1,41 +1,32 @@
 import React from 'react'
 import Link from 'next/link'
 import { Flex, Box, PseudoBox } from '@blockstack/ui'
+
+import { App } from '@models/app'
 import { Arrow } from '@components/arrow'
 
-export interface App {
-  name: string;
-  imgUrl?: string;
-  id: number;
-}
+const AppDirectoryItem: React.FC<{ app: App }> = ({ app }) => (
+  <Flex key={app.id}>
+    <Link href={`/maker/apps/${app.id}`}>
+      <PseudoBox display="flex" width={['320px', '432px']} maxWidth={432} mb={2} height={96} borderRadius={6} justifyContent="space-between" alignItems="center" background="#F9F9FC" _hover={{ bg: '#F0F0F5' }}>
+        <Box ml={6}>
+          <img src={app.imageUrl} alt={`Logo of ${app.name}`} width="48px" height="48px" />
+        </Box>
+        <Box flex={1} ml={6}>{app.name}</Box>
+        <Box mr={6}>
+          <Arrow direction="right" />
+        </Box>
+      </PseudoBox>
+    </Link>
+  </Flex>
+)
 
 interface AppDirectoryProps {
   apps: App[];
 }
 
 export const AppDirectory: React.FC<AppDirectoryProps> = ({ apps }) => (
-
-  <>
-    {
-      apps.map(app => (
-        <Flex key={app.id}>
-          <Link href={`/maker/apps/${app.id}`}>
-            <PseudoBox display="flex" width={['320px', '432px']} maxWidth={432} mb={2} height={96} borderRadius={6} justifyContent="space-between" alignItems="center" background="#F9F9FC" _hover={{ bg: '#F0F0F5' }}>
-              <Box
-                width="48px"
-                height="48px"
-                background="black"
-                ml={6}
-              />
-              <Box flex={1} ml={6}>{app.name}</Box>
-              <Box mr={6}>
-                <Arrow direction="right" />
-              </Box>
-            </PseudoBox>
-          </Link>
-        </Flex>
-      ))
-    }
-  </>
+  <Box my={[2, 2, 6, 12]}>
+    { apps.map(app => <AppDirectoryItem app={app} />) }
+  </Box>
 )
-
