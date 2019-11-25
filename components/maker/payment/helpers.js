5b0b8d24237d68721c53023fc916a0897564310d
kyranjamie <kyran.burraston@gmail.com>
2019-11-25 12:04:10 +0100

    fix: Quick fix for modal scroll issue, Closes #699

--- components/maker/payment/helpers.js
+++ components/maker/payment/helpers.js
@@ -1,21 +1,21 @@
-import { address, networks } from 'bitcoinjs-lib'
-import * as c32Check from 'c32check'
-import memoize from 'lodash/memoize'
+import { address, networks } from 'bitcoinjs-lib';
+import * as c32Check from 'c32check';
+import memoize from 'lodash/memoize';
 
 export const validateBTC = memoize(addr => {
   try {
-    address.toOutputScript(addr, networks.bitcoin)
-    return true
+    address.toOutputScript(addr, networks.bitcoin);
+    return true;
   } catch (error) {
-    return false
+    return false;
   }
-})
+});
 
 export const validateSTX = memoize(addr => {
   try {
-    c32Check.c32addressDecode(addr)
-    return true
+    c32Check.c32addressDecode(addr);
+    return true;
   } catch (error) {
-    return false
+    return false;
   }
-})
+});
