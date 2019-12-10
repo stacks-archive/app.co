68eb5002139ecf79b0ce0421dd6acce1562c0eb7
kyranjamie <kyran.burraston@gmail.com>
2019-12-10 16:11:49 +0100

    fix: Window not opening on Safari Closes #745

--- components/maker/kyc/index.js
+++ components/maker/kyc/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import useVisibilityChange from 'use-visibility-change';
 import { Flex, Box } from 'blockstack-ui';
@@ -9,6 +9,8 @@ import { fetchApps } from '@stores/maker/actions';
 const Kyc = ({ app, user }) => {
   const [loading, setLoading] = useState(false);
   const [hasStartedKyc, setHasStartedKyc] = useState(false);
+  let syncKycUrl = null;
+  const [kycUrl, setKycUrl] = useState(null);
   const dispatch = useDispatch();
 
   useVisibilityChange({
@@ -18,22 +20,29 @@ const Kyc = ({ app, user }) => {
     }
   });
 
-  const initiateKYC = async () => {
-    setLoading(true);
-    setHasStartedKyc(true);
-    const url = `${process.env.API_SERVER}/api/maker/apps/initiate-kyc?appId=${
-      app.id
-    }`;
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: new Headers({
-        authorization: `Bearer ${user.jwt}`
-      })
-    });
-    const data = await response.json();
-    window.open(data.embedURL);
-    setLoading(false);
-  };
+  useEffect(() => {
+    async function getEmbed() {
+      setLoading(true);
+      const url = `${
+        process.env.API_SERVER
+      }/api/maker/apps/initiate-kyc?appId=${app.id}`;
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: new Headers({
+          authorization: `Bearer ${user.jwt}`
+        })
+      });
+      const data = await response.json();
+      //
+      // Some confusion here as despite calling setKycUrl()
+      // when reading this value from the initiateKyc fn
+      // it remains null
+      setKycUrl(data.embedURL);
+      syncKycUrl = data.embedURL;
+      setLoading(false);
+    }
+    getEmbed();
+  }, [app.id]);
 
   const buttonText = () => {
     if (loading) {
@@ -51,13 +60,16 @@ const Kyc = ({ app, user }) => {
             Verifying your identity helps keep App Mining secure and fight
             fraud. Your ID will never be shared.
           </MakerCardText>
-          <Button
-            onClick={initiateKYC}
-            mt={4}
-            disabled={app.hasCollectedKYC}
-          >
-            {buttonText()}
-          </Button>
+          <a href={syncKycUrl || kycUrl} target="_blank">
+            <Button
+              as="a"
+              // onClick={initiateKYC}
+              mt={4}
+              disabled={app.hasCollectedKYC}
+            >
+              {buttonText()}
+            </Button>
+          </a>
         </Box>
       </Flex>
     </>
